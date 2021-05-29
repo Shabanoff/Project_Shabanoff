8400e1ec2a2081e3830e81f3465e5035b2bc54f4
Vladyslav Shabanov <shabanoff025@gmail.com>
2021-05-29 16:44:18 +0300

    fix

--- src/main/java/controller/i18n/filter/EncodingFilter.java
+++ src/main/java/controller/i18n/filter/EncodingFilter.java
@@ -1,14 +1,18 @@
 package controller.i18n.filter;
 
+import controller.listener.UniquenessCheckSessionListener;
 import javax.servlet.*;
 import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class EncodingFilter implements Filter {
     private static final String ENCODING = "utf-8";
+    private final Logger logger = LogManager.getLogger(EncodingFilter.class);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+    logger.info("I`m a LOGGER!");
     }
 
     @Override
